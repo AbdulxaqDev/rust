7845dda603d04c9bcd2a5c9f6de1e77af51e3d2e
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-04-14 18:47:02 +0300

    feat: iteration: loop and its usage

---
+++ projects/loops/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let mut number = 0;
+
+    let end_loop_result = loop {
+        number += 1;
+        if number == 10 {
+            break number;
+        }
+    };
+
+    println!("end loop result {end_loop_result}");
+
+    let mut material = 0;
+    let target = 10;
+
+    'factory: loop {
+        let mut gas = 3;
+        println!("Material amount: {material}");
+        println!("Starting engine!");
+
+
+        loop {
+            println!("Gas left: {gas}");
+            if gas == 0 {
+                println!("Stopping engine!");
+                break;
+            }
+            if material == target {
+                println!("We got enough, stoping factory!");
+                break 'factory;
+            }
+            gas -= 1;
+            material += 1;
+        }
+    }
+    println!("Material are made: {material}");
+}
