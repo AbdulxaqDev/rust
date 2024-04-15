9ed2f9a04b12c8607996677ae6e0f1e41b1f4a84
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-04-15 18:24:06 +0300

    feat: mutable and immutable refs

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
-    let mut s = String::from("Brendan");
-
+    let mut s = String::from("hello");
     let r1 = &s;
-    let r2 = &mut s;
-    
-    println!("{}, {}", r1, r2);
+    let r2 = &s;
+
+    println!("{} and {}", r1, r2); 
+
+    let r3 = &mut s;
+    println!("{}", r3);
 }
