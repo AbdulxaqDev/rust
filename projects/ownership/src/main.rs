175c606d5a11a920d69c10a5452cd61c5987e3b9
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-04-15 15:07:50 +0300

    feat: ownership demonstration

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    let s1 = String::from("Hello");
-    let s2 = s1.clone();
+    let _s1 = gives_ownership();
 
-    println!("{}", s1);
+    let s2 = String::from("Hello");
+
+    let _s3 = takes_and_gives_back(s2);
+}
+
+fn gives_ownership() -> String {
+    let s = String::from("hehe");
+    s    
+}
+
+fn takes_and_gives_back(s: String) -> String {
+    s
 }
