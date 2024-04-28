f6008cb61ee784ffad2d76c3691ce46fcfa4dbce
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-04-28 17:24:19 +0300

    feat: apply multiple mthods

--- tasks/ch5/area.rs
+++ tasks/ch5/area.rs
@@ -1,4 +1,3 @@
-#[derive(Debug)]
 struct Rectangle {
     w: i32,
     h: i32,
@@ -10,8 +9,21 @@ impl Rectangle {
     }
 }
 
+impl Rectangle {
+    fn w(&self) -> bool {
+        self.w > 0
+    }
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.w > other.w && self.h > other.h
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { w: 30, h: 50 };
+    let rect2 = Rectangle { w: 60, h: 40 };
 
-    println!("The area is {}", rect1.area());
+    println!("Can hold: {}", rect1.can_hold(&rect2));
 }
