7230bc2310241c37f9cc47c6e66565a0b3bcf28a
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-04-28 15:27:02 +0300

    refactor: apply struct

--- tasks/ch5/area.rs
+++ tasks/ch5/area.rs
@@ -1,9 +1,14 @@
-fn main() {
-    let rect1 = (30, 50);
-
-    println!("The area is {}", area(rect1));
+struct Rectangle {
+    w: i32,
+    h: i32,
 }
 
-fn area(rect: (i32, i32)) -> i32 {
-    rect.0*rect.1
+fn main() {
+    let rect1 = Rectangle { w: 30, h: 50 };
+
+    println!("The area is {}", area(&rect1));
+}
+
+fn area(rect: &Rectangle) -> i32 {
+    rect.w * rect.h
 }
