12fe8d59cfa05b507d3be05b8848f641e0276fba
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-04-28 18:39:10 +0300

    feat: associated struct functions

--- tasks/ch5/area.rs
+++ tasks/ch5/area.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     w: i32,
     h: i32,
@@ -21,9 +22,20 @@ impl Rectangle {
     }
 }
 
+impl Rectangle {
+    fn square(side: i32) -> Self {
+        Self {
+            w: side,
+            h: side,
+        }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { w: 30, h: 50 };
     let rect2 = Rectangle { w: 60, h: 40 };
 
-    println!("Can hold: {}", rect1.can_hold(&rect2));
+    let sqr1 = Rectangle::square(30);
+
+    println!("Square: {:#?}", sqr1);
 }
