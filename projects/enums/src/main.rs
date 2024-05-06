66340690c38351aeb009a0a1921400391a086e55
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-05-06 10:09:46 +0300

    feat: set options type in enum

--- projects/enums/src/main.rs
+++ projects/enums/src/main.rs
@@ -1,26 +1,14 @@
 #[derive(Debug)]
-enum IpAddrKind {
-    v4,
-    v6,
-}
-
-#[derive(Debug)]
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
 fn main() {
 
-    let home = IpAddr {
-        kind: IpAddrKind::v4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddr::V4(String::from("127.0.0.1")); 
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::v6,
-        address: String::from("::1"),
-    };
+    let loopback = IpAddr::V6(String::from("::1")); 
 
     println!("Home IP: {:#?}", home);
     println!("LoopBack IP: {:#?}", loopback);
