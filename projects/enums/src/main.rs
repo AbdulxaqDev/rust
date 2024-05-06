fb710fdd75f7e0dbefa346630258fe70ee376446
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-05-06 09:56:28 +0300

    feat: use of stuct and enum together

--- projects/enums/src/main.rs
+++ projects/enums/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+enum IpAddrKind {
+    v4,
+    v6,
 }
+
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+
+    let home = IpAddr {
+        kind: IpAddrKind::v4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::v6,
+        address: String::from("::1"),
+    };
+
+    println!("Home IP: {:#?}", home);
+    println!("LoopBack IP: {:#?}", loopback);
+}
+
+
+
